f82ddcd1a1427c684fed358fde433ded32ab2f1c
Mitali Patel <mitalipatel2606@gmail.com>
2026-03-23 12:42:57 +0530

    added user and video model, included user-defined methods for the userSchema

---
+++ src/models/video.model.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+import { Schema } from mongoose;
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+const videoSchema = new mongoose.Schema({
+    videoFile: {
+        type: String, //cloudinary
+        required: true
+    },
+    thumbnail: {
+        type: String, //cloudinary
+        required: true
+    },
+    title: {
+        type: String, //cloudinary
+        required: true
+    },
+    description: {
+        type: String, //cloudinary
+        required: true
+    },
+    duration: {
+        Type: Number,
+        required: true
+    },
+    views: {
+        Type: Number,
+        defualt: 0
+    },
+    published: {
+        type: Boolean,
+        default: true
+    },
+    owner: {
+        type: Schema.Types.ObjectId, //cloudinary
+        required: true,
+        ref: "User"
+    }
+
+
+}, {
+    timestamps: true
+})
+videoSchema.plugin(mongooseAggregatePaginate);
+
+export const Video = mongoose.model("Video", videoSchema);
