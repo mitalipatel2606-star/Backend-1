1829f77cd475759e0ff548b55421ce3cf9857a34
Mitali Patel <mitalipatel2606@gmail.com>
2026-04-01 23:43:47 +0530

    add user registration controller

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,5 +1,9 @@
 import { asyncHandler } from "../utils/asyncHandler.js"
 import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.model.js"
+import { uploadOnCloudinary } from "../utils/cloudinary.js"
+import { ApiResponse } from "../utils/ApiResponse.js";
+
 const registerUser = asyncHandler(async (req, res) => {
     res.status(200).json({
         message: "message ok"
@@ -19,14 +23,50 @@ const registerUser = asyncHandler(async (req, res) => {
         throw new ApiError(400, "All fields are required")
     }
     //now check is the user already exists
+    //findOne method:finds the first user with the given username/parameter
+    //$:operator to check values
+    const existedUser = User.findOne({
+        $or: [{ username }, { email }]
+    })
+    if (existedUser) {
+        throw new ApiError(409, "User with email or username already exists")
+    }
+    //extract the avatar image to our local 
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    const coverImageLocalPath = req.files?.coverImage.path;
 
+    if (!avatarLocalPath) throw new ApiError(400, "Uploading avatar is mandatory")
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+    if (!avatar) throw new ApiError(400, "Avatar is rewuired")
+    const user = await User.create({
+        fullName, avatar: avatar.url,
+        coverImage: coverImage?.url || "",
+        email,
+        password,
+        username: username.toLowerCase()
+    })
+    //verify user creation 
+    const createdUser = await User.findById(user._id).select(//write about what is to be removed/ignored
+        //i.e. dont select these two params
+        "-password -refreshToken"
+    )
+    if (!createdUser) throw new ApiError(500, "Somewthing went wrong while registering the user")
+
+    return res.status(201).json(
+        new ApiResponse(200, createdUser, "User registered successfully")
+    )
 })
+
+
 //get user details from frontend
 //vlaidation- format not empty, etc
 //check if user already exists(unique email/username)
 //files exist or not(images,avatar)
-//create objects for db
+
 //upload them to cloudinary
+//create objects for db
+
 //remove passwrod and response token field
 //check for user creation
 //return response
