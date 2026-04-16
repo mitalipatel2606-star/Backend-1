0b0766be86338e0d4baee35bd7f6a04faae80fd1
Mitali Patel <mitalipatel2606@gmail.com>
2026-04-16 19:00:25 +0530

    add routes for user profile management, account updates, and watch history

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,17 @@
 import { Router } from "express"
-import { registerUser, loginUser, logoutUser, refreshAccessToken } from "../controller/user.controller.js";
+import {
+    registerUser,
+    loginUser,
+    logoutUser,
+    refreshAccessToken,
+    changePassword,
+    getCurrentUser,
+    updateAccountDetails,
+    updateUserAvatar,
+    updateUserCoverImage,
+    getUserChannelProfile,
+    getWatchHistory
+} from "../controller/user.controller.js";
 import { upload } from "../middleware/multer.middleware.js";
 import { verifyJWT } from "../middleware/auth.middleware.js"
 
@@ -18,9 +30,18 @@ router.route("/register").post(
         }
     ]),
     registerUser)
+
+//we use our user-made verifyJWT middleware to check whether or not the user is logged in
 router.route("/login").post(loginUser)
 router.route("/logout").post(verifyJWT, logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/chnage-password").post(verifyJWT, changePassword)
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+//upload.single to upload just one field
+router.route("/avtar").patch(verifyJWT, upload.single("avtar"), updateUserAvatar)
+router.route("/cover-image").patch(verifyJWT, upload.single("cover-image"), updateUserCoverImage)
 
-
+router.route("/c/:username").get((verifyJWT, getUserChannelProfile))
+router.route("/history").get(verifyJWT, getWatchHistory)
 export default router
